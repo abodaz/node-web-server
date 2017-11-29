bf5d4bd926c3dd5e165cc5a381a2d9ae02151b8d
Abdallah Al-Azzami <abodaz100@gmail.com>
2017-11-29 23:54:08 +0200

    Initial commit

---
+++ tserver.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var hbs = require('hbs');
+var fs = require('fs');
+
+var app = express();
+hbs.registerPartials(__dirname + '/html/partials');
+app.set('view engine', 'hbs');
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `time: ${now} ${req.method} ${req.url}`;
+
+    console.log('sever.log'+log +'\n');
+    next();
+});
+
+app.use((req,res,next)=>{
+    res.render('break');
+   // next();
+});
+
+hbs.registerHelper('year', () => {
+    return new Date().getFullYear();
+});
+
+app.get('/', (req, res) => {
+    res.render('home', {
+        pagename: 'home'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        pagename1: 'about'
+    });
+});
+
+app.listen(8888);
