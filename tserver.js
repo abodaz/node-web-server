c9017516eee591df5bda99dd39bd1c0701c3d4a6
Abdallah Al-Azzami <abodaz100@gmail.com>
2017-11-30 00:52:40 +0200

    c4

--- tserver.js
+++ tserver.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var hbs = require('hbs');
 var fs = require('fs');
-const port = process.env.PORT || 8888;
+const port = process.set.port || 8888;
 
 var app = express();
 hbs.registerPartials(__dirname + '/html/partials');
