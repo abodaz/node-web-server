76e630d434634c12c9e3d214e65d0a52b3f17e1f
Abdallah Al-Azzami <abodaz100@gmail.com>
2017-12-14 00:49:20 +0200

    clerkAndManager

--- tserver.js
+++ tserver.js
@@ -11,22 +11,22 @@ const port = process.env.PORT || 8888;
 
 var app = express();
 
- app.use(express.static(__dirname+'/views'));
-app.use(express.static(__dirname+'/adminViews'));
+app.use(express.static(__dirname + '/views'));
+app.use(express.static(__dirname + '/adminViews'));
 
 
 //connectToDb();
-var config = 
-{
-  user:'root',
-  password: '2143906',
-  host: 'localhost', // update me
-  database:'gym',
-  port:3306
-};
+var config =
+    {
+        user: 'root',
+        password: '2143906',
+        host: 'localhost', // update me
+        database: 'gym',
+        port: 3306
+    };
 var connection = mysql.createConnection(config);
-connection.connect(function(err){
-    if(err) console.error(err);
+connection.connect(function (err) {
+    if (err) console.error(err);
     else console.log('Connected!');
 });
 
@@ -36,7 +36,7 @@ connection.connect(function(err){
 });*/
 
 app.use(require('body-parser')());
-app.use(express.static(__dirname+'/views'));
+app.use(express.static(__dirname + '/views'));
 hbs.registerPartials(__dirname + '/html/partials');
 app.set('view engine', 'hbs');
 app.use((req, res, next) => {
@@ -44,7 +44,7 @@ app.use((req, res, next) => {
     var log = `time: ${now} ${req.method} ${req.url}`;
 
     console.log('Hi Iam jayyab :)');
-    console.log('sever.log'+log +'\n');
+    console.log('sever.log' + log + '\n');
     next();
 });
 
@@ -60,7 +60,7 @@ hbs.registerHelper('year', () => {
 app.get('/', (req, res) => {
     res.render('home', {
         pagename: 'home'
-    });    
+    });
 });
 
 // app.get('/about', (req, res) => {
@@ -75,56 +75,104 @@ app.get('/', (req, res) => {
 //     });
 // });
 
-app.get('/login',(req,res)=>{
-    res.render('login',{
+app.get('/login', (req, res) => {
+    res.render('login', {
         pagename: 'login'
     });
 });
 
-app.get('/signup',(req,res)=>{
-    res.render('signup',{
+app.get('/signup', (req, res) => {
+    res.render('signup', {
         pagename: 'sign up'
     });
 });
-// app.get('/admin',(req,res)=>{
-//     res.render('admin',{
+// //admin
+// app.get('/admin', (req, res) => {
+//     res.render('admin', {
 //         pagename: 'Admin Panel'
 //     });
 // });
 
-// app.get('/seebranches',(req,res)=>{
-//     res.render('seebranches',{
+// app.get('/seebranches', (req, res) => {
+//     res.render('seebranches', {
 //         pagename: 'Our Branches'
 //     });
 // });
 
-// app.get('/addBM',(req,res)=>{
-//     res.render('addBM',{
+// app.get('/addBM', (req, res) => {
+//     res.render('addBM', {
 //         pagename: 'Add Branch Manager'
 //     });
 // });
 
-// app.get('/edadmin',(req,res)=>{
-//     res.render('edadmin',{
+// app.get('/edadmin', (req, res) => {
+//     res.render('edadmin', {
 //         pagename: 'Admin Panel'
 //     });
 // });
-// app.get('/singleBranch',(req,res)=>{
-//     res.render('singleBranch',{
+// app.get('/singleBranch', (req, res) => {
+//     res.render('singleBranch', {
 //         pagename: 'Branch'
 //     });
 // })
-// app.get('/addB',(req,res)=>{
-//     res.render('addB',{
+// app.get('/addB', (req, res) => {
+//     res.render('addB', {
 //         pagename: 'Add Branch'
+//     });
+// });
+// //manager
+// app.get('/manager', (req, res) => {
+//     res.render('manager', {
+//         pagename: 'Manager Page'
+//     });
+// })
+// app.get('/seeusers', (req, res) => {
+//     res.render('seeusers', {
+//         pagename: 'Our Users'
+//     });
+// });
+// app.get('/addEmp', (req, res) => {
+//     res.render('addEmp', {
+//         pagename: 'Add Employees'
+//     });
+// });
+// app.get('/edmanager', (req, res) => {
+//     res.render('edmanager', {
+//         pagename: 'Edit Infomations'
+//     });
+// });
+// //clerk
+// app.get('/clerk', (req, res) => {
+//     res.render('clerk', {
+//         pagename: 'Clerk Page'
+//     });
+// });
+// app.get('/edclerk', (req, res) => {
+//     res.render('edclerk', {
+//         pagename: 'Clerk Page'
+//     });
+// });
+// app.get('/seeMemb', (req, res) => {
+//     res.render('seeMemb', {
+//         pagename: 'Our Members'
+//     });
+// });
+// app.get('/addMemb', (req, res) => {
+//     res.render('addMemb', {
+//         pagename: 'Add Members'
+//     });
+// });
+// app.get('/commitMemb', (req, res) => {
+//     res.render('commitMemb', {
+//         pagename: 'Commit Members'
 //     });
 // });
 
 
-app.post('/signup',function(req,res){
+app.post('/signup', function (req, res) {
     console.log('Info Taken');
     //console.log('Form (form querystring): ' + req.query.);
-    var name = req.body.name;    
+    var name = req.body.name;
     var pass = req.body.password;
     console.log('name : ' + req.body.name);
     console.log('password : ' + req.body.password);
@@ -132,72 +180,128 @@ app.post('/signup',function(req,res){
 
     console.log('Pass after md5 --> ' + passEncr);
     res.end('Thank you');
-    
-    addEmp(name,passEncr,connection);  
+
+    addEmp(name, passEncr, connection);
     //res.redirect(303,'File');  
 });
 
-app.post('/login',function(req,res){
+app.post('/login', function (req, res) {
     console.log('You have entered info to log in -----');
     var name = req.body.username;
     var pass = req.body.password;
 
     var passEncr = md5(pass);
-    console.log('Your name is --> '+name + ', and the password is --> '+pass);
+    console.log('Your name is --> ' + name + ', and the password is --> ' + pass);
     console.log('Password after encr is --> ' + passEncr);
-    connection.query("SELECT user_id from users where user_name = '"+name+"' and user_password = '"+passEncr+"';",
-        function(err,result){            
-            if(err) {
+    connection.query("SELECT user_id from users where user_name = '" + name + "' and user_password = '" + passEncr + "';",
+        function (err, result) {
+            if (err) {
                 console.error(err);
                 res.end('Error Info');
             }
             else {
                 console.log(result); // test the lenght .
-                if(result.length == 0){
+                if (result.length == 0) {
                     console.log('Error Info');
                     res.end('Error Info');
-                } 
+                }
                 else {
                     var id = result[0].user_id;
                     console.log(result[0].user_id);
-                    if(id == 1){
+                    if (id == 1) // for the only Admin
+                     {
                         res.render('admin');
-                        app.get('/admin',(req,res)=>{
-                            res.render('admin',{
+                        app.get('/admin', (req, res) => {
+                            res.render('admin', {
                                 pagename: 'Admin Panel'
                             });
                         });
-                        
-                        app.get('/seebranches',(req,res)=>{
-                            res.render('seebranches',{
+
+                        app.get('/seebranches', (req, res) => {
+                            res.render('seebranches', {
                                 pagename: 'Our Branches'
                             });
                         });
-                        
-                        app.get('/addBM',(req,res)=>{
-                            res.render('addBM',{
+
+                        app.get('/addBM', (req, res) => {
+                            res.render('addBM', {
                                 pagename: 'Add Branch Manager'
                             });
                         });
-                        
-                        app.get('/edadmin',(req,res)=>{
-                            res.render('edadmin',{
+
+                        app.get('/edadmin', (req, res) => {
+                            res.render('edadmin', {
                                 pagename: 'Admin Panel'
                             });
                         });
-                        app.get('/singleBranch',(req,res)=>{
-                            res.render('singleBranch',{
+                        app.get('/singleBranch', (req, res) => {
+                            res.render('singleBranch', {
                                 pagename: 'Branch'
                             });
                         });
-                        app.get('/addB',(req,res)=>{
-                            res.render('addB',{
+                        app.get('/addB', (req, res) => {
+                            res.render('addB', {
                                 pagename: 'Add Branch'
                             });
                         });
-                        
+
                     }
-                   // res.end('Thank you');
+
+                    if (id == 2) // for the first Branch Manager
+                    {
+                        res.render('manager');
+                        app.get('/manager', (req, res) => {
+                            res.render('manager', {
+                                pagename: 'Manager'
+                            });
+                        })
+                        app.get('/seeusers', (req, res) => {
+                            res.render('seeusers', {
+                                pagename: 'Our Users'
+                            });
+                        });
+
+                        app.get('/addEmp', (req, res) => {
+                            res.render('addEmp', {
+                                pagename: 'Add Employees'
+                            });
+                        });
+                        app.get('/edmanager', (req, res) => {
+                            res.render('edmanager', {
+                                pagename: 'Edit Infomations'
+                            });
+                        });
+
+                    }
+                     if (id == 3) // for the first clerk
+                     {
+                        app.get('/clerk', (req, res) => {
+                            res.render('clerk', {
+                                pagename: 'Clerk Page'
+                            });
+                        });
+                        app.get('/edclerk', (req, res) => {
+                            res.render('edclerk', {
+                                pagename: 'Clerk Page'
+                            });
+                        });
+                        app.get('/seeMemb', (req, res) => {
+                            res.render('seeMemb', {
+                                pagename: 'Our Members'
+                            });
+                        });
+                        app.get('/addMemb', (req, res) => {
+                            res.render('addMemb', {
+                                pagename: 'Add Members'
+                            });
+                        });
+                        app.get('/commitMemb', (req, res) => {
+                            res.render('commitMemb', {
+                                pagename: 'Commit Members'
+                            });
+                        });
+                     }
+                    // res.end('Thank you');
                 }
             }
         });
@@ -205,11 +309,11 @@ app.post('/login',function(req,res){
 })
 
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`server port is ${port}`)
 });
 
 
 
- 
+
 
